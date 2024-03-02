dc0b267bd20239bcebd682cb2112f2a993035a82
Milou Mohamed MilouMohamed <49353852+MilouMohamed@users.noreply.github.com>
2024-03-02 12:32:08 +0100

    Add files via upload

--- PROJECTS/Slider/Slider_Script.js
+++ PROJECTS/Slider/Slider_Script.js
@@ -2,37 +2,40 @@ window.onload = () => {
   let span_Next = document.querySelector(".prev");
   let span_Pev = document.querySelector(".next");
   let lesImaes = document.querySelectorAll(".slider-container img");
-  let pgCre = document.querySelector(".paginations");  
+  let pgCre = document.querySelector(".paginations");
+  let span_slide_nbr = document.getElementById("number-sider");
 
   // Les options
   let currentSlide = 0;
   lesImaes[currentSlide].className = "active";
+
   span_Next.style.cursor = "no-drop";
   let nbrImg = lesImaes.length;
+
+  span_slide_nbr.innerHTML = `Slider #${+currentSlide + 1} / ${nbrImg}`;
   addPagination();
   fun_pagination();
 
   span_Pev.onclick = () => {
     ++currentSlide;
-    prev_Next_Slider("P",null);
+    prev_Next_Slider("P", null);
   };
 
   span_Next.onclick = () => {
     --currentSlide;
-    prev_Next_Slider("M",null);
+    prev_Next_Slider("M", null);
   };
-  
-    pgCre.addEventListener("click",slider_on_click);
 
+  pgCre.addEventListener("click", slider_on_click);
 
-  function prev_Next_Slider(sign,pagin) {
+  function prev_Next_Slider(sign, pagin) {
+    if (pagin) currentSlide = pagin;
 
-    if(pagin) currentSlide=pagin;
+    span_slide_nbr.innerHTML = `Slider #${+currentSlide + 1} / ${nbrImg}`;
 
     fun_pagination();
     span_Pev.style.cursor = "pointer";
     span_Next.style.cursor = "pointer";
-
 
     if (sign === "P" && currentSlide >= nbrImg - 1) {
       currentSlide = nbrImg - 1;
@@ -53,18 +56,28 @@ window.onload = () => {
   }
 
   function fun_pagination() {
-    let lesPaginat = document.querySelectorAll(".paginations .ul-pagin li");
-
+    
+    if (currentSlide >= 0 && currentSlide < nbrImg) {
+      let li_s = Array.from(pgCre.children[0].children);
+      
+      li_s.forEach((li_elem) => {
+        li_elem.classList.remove("is-on");
+      }); 
+      li_s[currentSlide].classList.add("is-on");
+      
+      /*
+      // let lesPaginat = document.querySelectorAll(".paginations .ul-pagin li");
     if (currentSlide >= 0 && currentSlide < nbrImg) {
       lesPaginat.forEach((li_peg) => {
         li_peg.classList.remove("is-on");
       });
       lesPaginat[currentSlide].classList.add("is-on");
     }
+    */
+    }
   }
 
   function addPagination() {
-
     pgCre.innerHTML = "";
     let ul_ = document.createElement("ul");
     ul_.className = "ul-pagin";
@@ -72,7 +85,7 @@ window.onload = () => {
     for (let index = 0; index < nbrImg; index++) {
       let li_ = document.createElement("li");
       li_.innerHTML = index + 1;
-      li_.dataset.iidd=index;
+      li_.dataset.iidd = index;
       if (index === 0) li_.className = "is-on";
 
       ul_.appendChild(li_);
@@ -80,11 +93,9 @@ window.onload = () => {
     pgCre.appendChild(ul_);
   }
 
-function slider_on_click(eve){ 
-currentSlide=eve.target.dataset.iidd ;
-prev_Next_Slider("",currentSlide);
-    lesImaes[currentSlide].className = "active"; 
-}
-
+  function slider_on_click(eve) {
+    currentSlide = eve.target.dataset.iidd;
+    prev_Next_Slider("", currentSlide);
+  }
 };
 // Milou Med
